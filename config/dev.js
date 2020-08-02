0a36cb731f5d63fe151430744f3714c468934752
hardikshakya <hardikshakya1997@gmail.com>
2020-08-02 18:11:02 +0530

    T3: Wiring Up Continous Integration - Used Travis CI

--- config/dev.js
+++ config/dev.js
@@ -4,4 +4,5 @@ module.exports = {
   googleClientSecret: '<add your google client secret>',
   mongoURI: '<mongo db path to blog_dev database>',
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
